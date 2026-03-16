69c854d7b1141a6593464d5b81e3e1b77539fa0e
Astesia318 <99835649@qq.com>
2026-03-16 21:12:19 +0800

    Simple stock select algorithm - top 50 stock in all sector

--- config.py
+++ config.py
@@ -3,11 +3,11 @@
 # --- 数据与选股参数 ---
 # BASE_SECTOR = ''      # 基础股票池
 TOP_N_STOCKS = 50           # 最终保留的活跃股数量
-LOOKBACK_DAYS = 30          # 评估活跃度的回溯天数
+LOOKBACK_DAYS = 50          # 评估活跃度的回溯天数
 
 # --- 时间范围设定 ---
 RESEARCH_START_DATE = '20150101'
-RESEARCH_END_DATE = '20231231'
+RESEARCH_END_DATE = '20260313'
 
 # --- 路径配置 ---
 CSV_OUTPUT_DIR = './qlib_source_csvs'
